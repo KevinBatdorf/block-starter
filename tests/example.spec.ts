4693ba14246d52942cac4d225e7b4b0251fce197
Kevin Batdorf <kbat82@gmail.com>
2025-10-02 18:10:03 +0700

    build first

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,15 +6,9 @@ test.beforeEach(async ({ requestUtils }) => {
 });
 
 test("Block is added", async ({ admin, page, editor }) => {
-	// await admin.createNewPost({ title: "My first post" });
-	// Wait for editor iFrame to load
-	await admin.visitAdminPage("plugins.php");
-	// await page.waitForSelector("iframe[name='editor-canvas']", {
-	// 	timeout: 15_000,
-	// });
-	// // Just a dummy test to verify things are working
-	// await editor.insertBlock({ name: "kevinbatdorf/block-starter" });
-	// // This text would be in the sidebar
-	// await page.waitForSelector('[data-testid="coming-soon"]');
+	await admin.createNewPost({ title: "My first post" });
+	// Just a dummy test to verify things are working
+	await editor.insertBlock({ name: "kevinbatdorf/block-starter" });
+	// This text would be in the sidebar
 	expect(page.getByTestId("coming-soon")).toContainText("Coming soon");
 });
